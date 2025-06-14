384cd5920231a7f67445c3a7979ee151a409dd58
RemiSaurel <r.saurel5@gmail.com>
2025-06-14 12:36:48 +0200

    chore: tab title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: '🧑‍🏫 Evalbuddy',
+    },
+  },
   devtools: { enabled: true },
 
   modules: [
