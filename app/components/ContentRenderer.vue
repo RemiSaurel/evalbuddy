94c33db5426a207b15be0c98df7d22d76b688bba
RemiSaurel <r.saurel5@gmail.com>
2025-08-19 19:35:41 +0200

    chore: remove dead code

--- app/components/ContentRenderer.vue
+++ app/components/ContentRenderer.vue
@@ -17,15 +17,3 @@ const processedContent = computed(() => processContent(props.content))
     v-html="processedContent"
   />
 </template>
-
-<style>
-/* Essential KaTeX styles that can't be replaced with Tailwind */
-.katex-display {
-  margin: 1em 0;
-  text-align: center;
-}
-
-.katex {
-  font-size: 1em;
-}
-</style>
